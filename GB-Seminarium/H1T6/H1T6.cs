6d2028e2716c1157957a958a1f586b65d2c5bf67
Crypt0head <zelas.money@yandex.ru>
2022-08-14 19:29:14 +0300

    Homework 1 Task 6

---
+++ GB-Seminarium/H1T6/H1T6.cs
@@ -0,0 +1,13 @@
+﻿void IsEven(int value){
+    
+    if(value % 2 == 0){
+        Console.WriteLine("yes");
+    }
+    else{
+        Console.WriteLine("no");
+    }
+}
+
+Console.Write("Enter the value: ");
+
+IsEven(int.Parse(Console.ReadLine()));
