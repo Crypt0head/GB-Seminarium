ac744125403854f78b81da7248a89f026650ac88
Crypt0head <zelas.money@yandex.ru>
2022-08-16 19:59:54 +0300

    Homework 2 Task 10

---
+++ H2T10/H2T10.cs
@@ -0,0 +1,6 @@
+﻿int GetSecondDigit(int value){
+    return value/10%10;
+}
+
+Console.Write("Input three-digit number: ");
+Console.WriteLine("Output: {0}", GetSecondDigit(int.Parse(Console.ReadLine())));
