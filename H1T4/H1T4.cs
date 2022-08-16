1474920722a291c61b66b722965f3f748fd03884
Crypt0head <zelas.money@yandex.ru>
2022-08-16 20:44:03 +0300

    Tasks descriptions added to the source files

--- H1T4/H1T4.cs
+++ H1T4/H1T4.cs
@@ -1,4 +1,10 @@
-﻿int Maxof3(int value1, int value2, int value3){
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int Maxof3(int value1, int value2, int value3){
     int result = value1;
 
     if(value2>result){
