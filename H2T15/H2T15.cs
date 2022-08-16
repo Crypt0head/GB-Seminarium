ec434ce603a3a15027112306fdce85f2664e4f11
Crypt0head <zelas.money@yandex.ru>
2022-08-16 20:35:14 +0300

    Homework 2 Task 15

---
+++ H2T15/H2T15.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+void IsWeekend(int val){
+    if(val > 5 && val < 8){
+        Console.WriteLine("Yes");
+    }
+    else{
+        Console.WriteLine("No");
+    }
+}
+
+Console.Write("Input digit: ");
+IsWeekend(int.Parse(Console.ReadLine()));
