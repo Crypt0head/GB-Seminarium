7b88445292c49ce2d34d84292c2d8830b93790e6
Crypt0head <zelas.money@yandex.ru>
2022-08-18 18:50:02 +0300

    Homework 3 Task 23

---
+++ H3T23/H3T23.cs
@@ -0,0 +1,20 @@
+﻿// Задача 23
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+using System;
+
+void PrintCubeTable(int val)
+{
+    for(int i=1;i<=val;i++)
+    {
+        Console.Write("{0} ", Convert.ToInt32(Math.Exp(3*Math.Log(i))));
+    }
+
+    Console.WriteLine();
+}
+
+Console.Write("Input N: ");
+PrintCubeTable(int.Parse(Console.ReadLine()));
