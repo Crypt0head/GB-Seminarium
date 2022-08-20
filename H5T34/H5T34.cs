35c34a7eea9f683e378c4efecdc0ddb9dd948867
Crypt0head <zelas.money@yandex.ru>
2022-08-20 16:20:10 +0300

    Homework 5 Task 34

---
+++ H5T34/H5T34.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] GetRandomArray(int n)
+{
+    int[] res = new int[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        res[i] = new Random().Next(100, 1000);
+    }
+
+    return res;
+}
+
+void ShowEvenNumbers(int[] arr)
+{
+    foreach(int i in arr)
+    {
+        if(i%2 == 0)
+        {
+            Console.Write("{0} ", i);
+        }
+    }
+
+    Console.WriteLine();
+}
+
+Console.Write("Input array size: ");
+ShowEvenNumbers(GetRandomArray(int.Parse(Console.ReadLine())));
