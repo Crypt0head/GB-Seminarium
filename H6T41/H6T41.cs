1abdbe63ab5e13e13fcb6aa6c337a0d3a2c42952
Crypt0head <zelas.money@yandex.ru>
2022-08-24 16:26:27 +0300

    Homework 6 Task 41

---
+++ H6T41/H6T41.cs
@@ -0,0 +1,36 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] SetArray(int m)
+{
+    int[] res = new int[m];
+    
+    for(int i=0;i<res.Length;i++)
+    {
+        Console.Write("Input [{0}] element: ", i+1);
+        res[i] = int.Parse(Console.ReadLine());
+    }
+
+    return res;
+}
+
+int GetCount(int[] arr)
+{
+    int res = 0;
+
+    foreach(int i in arr)
+    {
+        if(i > 0)
+        {
+            res++;
+        }
+    }
+
+    return res;
+}
+
+Console.Write("Input array size: ");
+Console.Write("Output: {0} \n", GetCount(SetArray(int.Parse(Console.ReadLine()))));
