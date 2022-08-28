4534287520d6d1ce9c84f8b3d9c25ce603b46c9a
Crypt0head <zelas.money@yandex.ru>
2022-08-28 20:26:44 +0300

    Homework 7 Task 52

---
+++ H7T52/H7T52.cs
@@ -0,0 +1,46 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] MakeRandomArray(int n, int m)
+{
+    int[,] arr = new int[n,m];
+
+    for(int i=0;i<n; i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            arr[i,j] = new Random().Next(-10, 11);
+            Console.Write("{0}, ", arr[i,j]);
+        }
+        Console.WriteLine();
+    }
+
+    return arr;
+}
+
+void ShowAvrOfRows(int[,] arr)
+{
+    int rows = arr.GetUpperBound(0) + 1;
+    int columns = arr.Length / rows;
+
+    for(int i=0;i<rows;i++)
+    {
+        double res = 0;
+        for(int j=0;j<columns;j++)
+        {
+            res+=arr[i,j];
+        }
+        res/=columns;
+        Console.Write("{0}, ", res);
+    }
+}
+
+
+Console.WriteLine("Input array size NxM: ");
+
+ShowAvrOfRows(MakeRandomArray(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
