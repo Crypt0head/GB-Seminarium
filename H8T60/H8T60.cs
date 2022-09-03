9956a633885180fb51eb10acbabb89f320bd1c0b
Crypt0head <zelas.money@yandex.ru>
2022-09-03 17:56:13 +0300

    Homework 8 Task 60

--- H8T60/H8T60.cs
+++ H8T60/H8T60.cs
@@ -6,3 +6,65 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
+int[,,] MakeRandom3DArray(int row, int col, int z)
+{
+    if(row*col*z>=90)
+    {
+        Console.WriteLine("ERROR: The array could contain only unique 2-digit numbers, which means not more than 90 members.\nPlease try another input.");
+        return new int[0,0,0];
+    }
+
+    int[,,] arr = new int[row,col,z];
+    List<int> list = new List<int>();
+    bool unique = false;
+
+    for(int i=0;i<row; i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            for(int k=0, tmp=0;k<z;k++)
+            {
+                unique = false;
+                while(!unique)
+                {
+                    tmp = new Random().Next(10, 100);
+                    if(!list.Contains(tmp))
+                    {
+                        arr[i,j,k] = tmp;
+                        list.Add(tmp);
+                        unique = true;
+                    }
+                       
+                }
+            }
+        }
+    }
+
+    return arr;
+}
+
+void Print3DArray(int[,,] arr)
+{
+    if(arr.Length == 0)
+        return;
+        
+    int rows = arr.GetUpperBound(0)+1;
+    int columns = arr.GetUpperBound(1)+1;
+    int z = arr.GetUpperBound(2)+1;
+
+    for(int i=0;i<z; i++)
+    {
+        for(int j=0;j<rows;j++)
+        {
+            for(int k=0;k<columns;k++)
+            {
+                Console.Write($"{arr[j,k,i]}({j},{k},{i}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+Console.WriteLine("Enter dimansions NxMxL: ");
+int[,,] arr = MakeRandom3DArray(int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine()),int.Parse(Console.ReadLine()));
+Print3DArray(arr);
