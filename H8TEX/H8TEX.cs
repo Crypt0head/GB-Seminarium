1c1b450beeecc480c00be016278ab9df9626eef3
Crypt0head <zelas.money@yandex.ru>
2022-09-03 04:25:58 +0300

    Homework 8 Task Extra

---
+++ H8TEX/H8TEX.cs
@@ -0,0 +1,24 @@
+﻿// Задача со звездочкой:
+// Написать функцию Sqrt(x) - квадратного корня, которая принимает на вход целочисленное 
+// значение x и возвращает целую часть квадратного корня от введенного числа.
+
+// 4 -> 2
+// 28 -> 5
+
+// Нельзя использовать встроенные функции библиотеки Math!
+
+int sqrt(int val)
+{
+    double x = val/2, a;
+    int res = 0;
+    while(res!=System.Convert.ToInt32(x-x%1))
+    {
+        res=System.Convert.ToInt32(x-x%1);
+        a=(val-x*x)/(2*x);
+        x = (x+a)-(a*a)/(2*(x+a));
+    }
+    return res;
+}
+
+Console.Write("Enter number: ");
+Console.WriteLine("Output: {0}", sqrt(int.Parse(Console.ReadLine())));
