61794cfad05e49b33a1666bf37ffbbb4475a4c93
Crypt0head <zelas.money@yandex.ru>
2022-09-08 19:34:15 +0300

    Homework 9 Task 66

---
+++ H9T66/H9T66.cs
@@ -0,0 +1,17 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Enter two numbers as first and last range values: ");
+double m = Double.Parse(Console.ReadLine());
+double n = Double.Parse(Console.ReadLine());
+
+double res = 0;
+
+for(double i = Math.Round(m+0.4, MidpointRounding.AwayFromZero);i<=Math.Round(n, MidpointRounding.ToZero);i++)
+{
+    res+=i;
+}
+
+Console.WriteLine("Output: {0}", res);
